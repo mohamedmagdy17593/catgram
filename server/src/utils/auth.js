92a69104dbe8c4f30bcf8cc649b512c35a31c333
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2020-05-23 01:46:55 +0200

    simple refactor

--- server/src/utils/auth.js
+++ server/src/utils/auth.js
@@ -2,8 +2,9 @@ import { prisma } from '../prisma'
 
 export async function getAuthCat(req) {
   let id = +req.signedCookies.catId
-
-  let cat = await prisma.cat.findOne({ where: { id } })
-
-  return cat
+  if (id) {
+    let cat = await prisma.cat.findOne({ where: { id } })
+    return cat
+  }
+  return null
 }
